442fb2b1df2674a8caf05f5e3292c5dae3b2469f
1985makarovdenis1985 <makarovdenis07081985@gmail.com>
2023-04-24 13:05:47 -0600

    step back

--- config/config.js
+++ config/config.js
@@ -1,28 +1,28 @@
 const {Sequelize} = require('sequelize')
 
-// module.exports = new Sequelize(
-//     'postgres',
-//     'postgres',
-//     '12341234',
-//
-//     {
-//         dialect: 'postgres',
-//         host: 'hatchdatabase.cdqbqb5oryfn.us-east-2.rds.amazonaws.com',
-//         port: 5432,
-//     }
-// )
-
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    'postgres',
+    'postgres',
+    '12341234',
 
     {
         dialect: 'postgres',
-        host: process.env.DB_URL,
-        port: process.env.DB_PORT,
+        host: 'hatchdatabase.cdqbqb5oryfn.us-east-2.rds.amazonaws.com',
+        port: 5432,
     }
 )
+
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_URL,
+//         port: process.env.DB_PORT,
+//     }
+// )
 
 
 
